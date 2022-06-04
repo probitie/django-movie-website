eae337a51212ad492347bb0d6c4535cfce3faa11
probitiye <vm683497580@gmail.com>
2022-06-04 22:01:06 +0300

    made detail view and main page

---
+++ django_movie/movies/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from .models import Movie
+
+class MovieView(View):
+    """Список фильмов"""
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    """Подробное описание фильма""" 
+
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, "movies/movie_detail.html", {"movie": movie})
