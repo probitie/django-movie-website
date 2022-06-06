1c82c881ba50605fb66f6a5f31598c1f4937897c
probitiye <vm683497580@gmail.com>
2022-06-06 12:35:48 +0300

    added movie shots and reviews output, created base template

--- django_movie/movies/urls.py
+++ django_movie/movies/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.MovieView.as_view(), name="movies"),
-    path('<slug:slug>/', views.MovieDetailView.as_view(), name="movie_detail")
+    path('<slug:slug>/', views.MovieDetailView.as_view(), name="movie_detail"),
+    path('review/<int:pk>/', views.AddReview.as_view(), name="add_review"),
+
 ]
